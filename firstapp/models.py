4fadfd6bfc9ff3f1c66872b766ac6c7713839437
Ramachandra-2096 <ramachandraudupa2004@gmail.com>
2023-11-29 21:38:34 +0530

    updated login/sighup mechanism

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,18 @@
+# myapp/models.py
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import F
 
-# Create your models here.
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    projects =models.CharField()
+    date = models.DateTimeField()
+    def __str__(self):
+        return self.user.username
+
+class Create_company(models.Model):
+    Company_name =models.CharField(max_length=225)
+    GSTIN =models.CharField(max_length=225)
+    Created_date =models.DateField()  
+
+    
